2457160c5a82aa5d2145002fe06a6808862750ad
praneethkumarpidugu <pidugu2007@gmail.com>
2018-02-10 22:30:52 -0500

    solved using algorithmic approach using the following - toString to convert number to string -parseInt to convert back to integer

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,34 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  correctSign = Math.sign(n)
+  reversedInt = '';
+  if(correctSign == 1){
+    for(let number of n.toString().split(''))
+      {
+        if(Math.sign(number) == 0){
+          continue
+        }
+        reversedInt = number + reversedInt;
+      }
+    return parseInt(reversedInt);
+  }
+  else if (correctSign == -1)
+  {
+    for(let number of n.toString().split(''))
+      {
+        if(Math.sign(number) == 0){
+          continue
+        }
+        reversedInt = number + reversedInt;
+  }
+
+    return parseInt(reversedInt) * correctSign;
+  } else {
+    return n;
+  }
+
+}
 
 module.exports = reverseInt;
