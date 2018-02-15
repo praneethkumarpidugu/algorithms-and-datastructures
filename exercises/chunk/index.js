db6143dbf9bfdf2e847cf71728b56019d6ded38d
praneethkumarpidugu <pidugu2007@gmail.com>
2018-02-15 04:25:30 -0500

    array chunk solution # 1

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,18 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  var arr_list = [];
+  for (var i=0; i<array.length;)
+  {
+    var temp_arr_list = [];
+    while (temp_arr_list.length <size && i < array.length){
+        temp_arr_list.push(array[i]);
+        i++;
+    }
+    arr_list.push(temp_arr_list);
+  }
+  return arr_list;
+}
 
 module.exports = chunk;
