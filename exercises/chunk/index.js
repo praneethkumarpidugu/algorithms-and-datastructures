69b575428d0178668add66aace7f38970ee9d4b0
praneethkumarpidugu <pidugu2007@gmail.com>
2018-02-16 02:40:55 -0500

    Solution # 3 using slice

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -26,19 +26,33 @@
 
 // Solution # 2
 
+// function chunk(array, size)
+// {
+//   const chunked = [];
+//   for (let element of array)
+//   {
+//     const last = chunked[chunked.length - 1];
+//     if (!last || last.length === size)
+//     {
+//       chunked.push([element]);
+//     }
+//     else {
+//       last.push(element);
+//     }
+//   }
+//   return chunked;
+// }
+
+//  Solution # 3
+
 function chunk(array, size)
 {
   const chunked = [];
-  for (let element of array)
+  var index = 0;
+  while (index < array.length)
   {
-    const last = chunked[chunked.length - 1];
-    if (!last || last.length === size)
-    {
-      chunked.push([element]);
-    }
-    else {
-      last.push(element);
-    }
+    chunked.push(array.slice(index, index + size));
+    index = index + size;
   }
   return chunked;
 }
